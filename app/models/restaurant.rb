47faba4b82fe70134d55e7160e24919bc5dde16b
Marina Stepanova <stepanov.mari@gmail.com>
2023-05-24 14:59:35 +0200

    oh god

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,7 @@
 class Restaurant < ApplicationRecord
-  has_many :reviews
-  validates :name, presence:true
-  validates :address, presence:true
-  validates :category, presence:true
+  has_many :reviews, dependent: :destroy
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true
+  validates :category, inclusion: { in: %w[chinese italian japanese french belgian] }
 end
